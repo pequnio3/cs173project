bad401495cb67c109c0c721f8a8dabda7b4b8081
pequnio3 <pequino3@gmail.com>
2013-03-06 18:51:56 -0800

    fixed kmer counting

--- experiments/src/kmers.py
+++ experiments/src/kmers.py
@@ -1,11 +1,30 @@
 #!/usr/bin/python
 
+def computeReverseCompliment(s):
+    rc_s = s
+    rc_s = rc_s.replace("A","1")
+    rc_s = rc_s.replace("T","A")
+    rc_s = rc_s.replace("1","T")
+    rc_s = rc_s.replace("G","1")
+    rc_s = rc_s.replace("C","G")
+    rc_s = rc_s.replace("G","T")
+    rc_s = rc_s[::-1]
+    return rc_s
+
 def computeKmerCounts(s,k):
-    kmer_counts={}
-    for i in xrange(0,len(s)-k):
-        kmer = s[i:i+k]
-        if kmer in kmer_counts:
-            kmer_counts[kmer] = kmer_counts[kmer] + 1
-        else:
-            kmer_counts[kmer] = 1 
+    kmer_counts ={}
+    rc_s = computeReverseCompliment(s)
+    print rc_s
+    print s
+    parts = [s]
+    if rc_s != s:
+        parts.append(rc_s)
+        
+    for part in parts:
+        for i in range(0,len(part)+1-k):
+            kmer = s[i:i+k]
+            if kmer in kmer_counts:
+                kmer_counts[kmer] = kmer_counts[kmer] + 1
+            else:
+                kmer_counts[kmer] = 1
     return kmer_counts
