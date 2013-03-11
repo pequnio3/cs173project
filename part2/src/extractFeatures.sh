cff3ccc9140896c56ee601c908d39c747e371496
pequnio3 <pequnio3@gmail.com>
2013-03-11 00:05:02 -0700

    updated extract features

--- part2/src/extractFeatures.sh
+++ part2/src/extractFeatures.sh
@@ -1,50 +1,35 @@
 #!/bin/bash                                                                                                                                                                                              
 
 if [ $# -ne 4 ]
-    then
-    echo "usage: extractFeatures data_dir vista.pickle chrom.sizes body_part"
+then
+    echo "usage: extractFeatures feature_dir regions_file chrom.sizes wig_file"
 else
 
-    data_dir=$1
-    vista_pickle=$2
+    feature_dir=$1
+    regions_file=$2
     chrom_sizes=$3
-    body_part=$4
+    wigs_file=$4
 
-    bp_dir=$data_dir/$body_part
-    echo $bp_dir
 
-    
-    labels=$bp_dir/$body_part.labels
 
-    wig=$bp_dir/wig.wig
-    bigwig=$bp_dir/bigwig.bw
-    wigs=`cat ../data/brain_wigs.txt`
-    out=$bp_dir/$body_part.fv
-    features=$bp_dir/temp.fv
-    regions=$bp_dir/regions.bed
-    rm -rf $bp_dir
-    mkdir $bp_dir
 
-    python generateLabels.py $vista_pickle $body_part $out
-    python generateBed.py $vista_pickle $regions
+    wigs=`cat $wigs_file`
 
- 
-
-    count=0
     for wig_loc in $wigs
-      do
+    do
+	feature_name=`basename $wig_loc | sed s/.wig.gz//`
 
-       wget -O $wig.gz $wig_loc
-       gunzip $wig.gz
+	wig=$feature_dir/$feature_name.wig
+	bigwig=$feature_dir/$feature_name.bg
 
-       /afs/ir/class/cs173/bin/i386_linux26/wigToBigWig $wig $chrom_sizes $bigwig
-       rm $wig
-       /afs/ir/class/cs173/bin/i386_linux26/bigWigAverageOverBed $bigwig $regions $features
-       
-       paste -d ' '  $out <(cat $features | awk 'BEGIN{count='$count'*5}{ printf("%i:%f %i:%f %i:%f %i:%f %i:%f\n",count,$2,count+1,$3,count+2,$4,count+3,$5,count+4,$6) }') > $out.temp
-       mv $out.temp $out
-       rm $features
-       count=$count+1
+	features=$features_dir/$feature_name.fv
+
+	wget -O $wig.gz $wig_loc
+	gunzip $wig.gz
+
+	/afs/ir/class/cs173/bin/i386_linux26/wigToBigWig $wig $chrom_sizes $bigwig
+	rm $wig
+	/afs/ir/class/cs173/bin/i386_linux26/bigWigAverageOverBed $bigwig $regions_file $features
+	rm $bigwig
     done
-
 fi
