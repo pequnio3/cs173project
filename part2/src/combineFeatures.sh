ce164ff7950ae4c93f4230255a379f75f3647106
pequnio3 <pequino3@gmail.com>
2013-03-11 12:51:51 -0700

    updated combine Features

--- part2/src/combineFeatures.sh
+++ part2/src/combineFeatures.sh
@@ -2,17 +2,16 @@
 
 if [ $# -ne 4 ]
 then
-    echo "usage: combineFeatures data_dir wigs_file vista.pickle body_part features_dir"
+    echo "usage: combineFeatures data_dir vista.pickle body_part features_dir"
 else
     
     data_dir=$1
-    wig_file=$2
-    vista_pickle=$3
-    body_part=$4
-    features_dir=$5
+    vista_pickle=$2
+    body_part=$3
+    features_dir=$4
 
     # generate a 
-    wigs=`fn $wigs_file $body_part`
+    features=`ls $features_dir`
 
     bp_dir=$data_dir/$body_part
     echo $bp_dir
@@ -21,21 +20,19 @@ else
     labels=$bp_dir/$body_part.labels
 
     out=$bp_dir/$body_part.fv
-    touch $out
 
     rm -rf $bp_dir
     mkdir $bp_dir
 
+    touch $out
+
     python generateLabels.py $vista_pickle $body_part $out
 
     count=0
-    for wig_loc in $wigs
+    for feature in $features
     do
-	feature_name=`basename $wig_loc | sed s/.wig.gz//`
-	features=$features_dir/$feature_name.fv
-	paste -d ' '  $out <(cat $features | awk 'BEGIN{count='$count'*5}{ printf("%i:%f %i:%f %i:%f %i:%f %i:%f\n",count,$2,count+1,$3,count+2,$4,count+3,$5,count+4,$6) }') > $out.temp
+	paste -d ' '  $out <(cat $features_dir/$feature | awk 'BEGIN{count='$count'*5}{ printf("%i:%f %i:%f %i:%f %i:%f %i:%f\n",count,$2,count+1,$3,count+2,$4,count+3,$5,count+4,$6) }') > $out.temp
 	mv $out.temp $out
-	rm $features
 	count=$count+1
     done
 
