b0033c21b809ced3c5d9e8329486bb6f03195fc1
pequnio3 <pequino3@gmail.com>
2013-03-11 12:52:02 -0700

    updated extractFeatures for part 2

--- part2/src/extractFeatures.sh
+++ part2/src/extractFeatures.sh
@@ -2,11 +2,11 @@
 
 if [ $# -ne 4 ]
 then
-    echo "usage: extractFeatures feature_dir regions_file chrom.sizes wig_file"
+    echo "usage: extractFeatures feature_dir expressors.bed chrom.sizes wigs_file"
 else
 
-    feature_dir=$1
-    regions_file=$2
+    features_dir=$1
+    expressors_bed=$2
     chrom_sizes=$3
     wigs_file=$4
 
@@ -18,18 +18,36 @@ else
     for wig_loc in $wigs
     do
 	feature_name=`basename $wig_loc | sed s/.wig.gz//`
+	echo "***************"
+	echo "extracting: ${feature_name}"
+	echo "***************"
 
-	wig=$feature_dir/$feature_name.wig
-	bigwig=$feature_dir/$feature_name.bg
+	wig=$features_dir/$feature_name.wig
+	bigwig=$features_dir/$feature_name.bg 
 
 	features=$features_dir/$feature_name.fv
 
-	wget -O $wig.gz $wig_loc
-	gunzip $wig.gz
+	echo "downlaoding ${wig}.gz"
+	
+	wget -q -O $wig.gz $wig_loc
 
-	/afs/ir/class/cs173/bin/i386_linux26/wigToBigWig $wig $chrom_sizes $bigwig
-	rm $wig
-	/afs/ir/class/cs173/bin/i386_linux26/bigWigAverageOverBed $bigwig $regions_file $features
-	rm $bigwig
+	if [ -s $wig.gz ]
+	then
+	    
+	    echo "extracting ${wig}.gz"
+	    gunzip $wig.gz
+	    echo "generating big wig ${bigwig}"
+	    /afs/ir/class/cs173/bin/i386_linux26/wigToBigWig $wig $chrom_sizes $bigwig
+	    rm $wig
+
+	    echo "generating features  ${features}"
+	    /afs/ir/class/cs173/bin/i386_linux26/bigWigAverageOverBed $bigwig $expressors_bed $features
+	    rm $bigwig
+	else
+	    echo "xxxxxxx - downloading ${wig}.gz failed."
+	    echo "continuing"
+	    rm $wig.gz
+	fi
+
     done
 fi
